9d6ce9c24b5022cdd6a93a9beff7b000278b72b2
Emilie Montredon <montredon.emilie@gmail.com>
2016-03-13 11:11:40 +0100

    updated start-app helper to use local parse-server

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -5,7 +5,14 @@ import config from '../../config/environment';
 export default function startApp(attrs) {
   let application;
 
-  var attributes = Ember.merge({}, config.APP);
+  let attributes = Ember.merge(
+    {
+      parseUrl        : 'http://localhost:1337',
+      parseNamespace  : 'parse',
+      applicationId   : '6ZNbAMXjYcg8BLakQ8AVyFWyfA6cTWwPwLb2Gzii',
+      restApiId       : 'E2gCZw7WFgz3jfXhxjz9YnconZrbkTDgCUpLFQqy'
+    },
+  config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
   Ember.run(() => {
